0f784206f8158126f4aa32d4c3c15029bdca38f7
Mateusz Jamrowski <kermaxpl@gmail.com>
2023-08-14 22:48:08 +0200

    Initial MQTT implementation

--- client.py
+++ client.py
@@ -1,7 +1,146 @@
-from rMoistPi.socket_client import SocketClient
-from rMoistPi.message.moisture_message import MoistureMessage
+import os
+from dotenv import dotenv_values
+import random
 
-client = SocketClient("127.0.0.1")
+from functools import reduce 
+
+from typing import Optional
+
+from paho.mqtt import client as mqtt_client
+
+import socket
+import ssl
+
+import logging
+
+import time
+
+import threading
+
+logging.basicConfig(level=logging.INFO)
+
+def coalesce(*arg):
+  return reduce(lambda x, y: x if x is not None else y, arg)
+
+#TODO: project cleanup
+
+class ClientConfig:
+    DEF_PORT = 1883
+    DEF_PREFIX = "home/iot/"
+
+    def __init__(self, 
+                 broker: Optional[str] = None, 
+                 port: Optional[int] = None, 
+                 user: Optional[str] = None, 
+                 password: Optional[str] = None):
+        
+        config = {
+            **os.environ,
+            **dotenv_values(".env")
+        }
+
+        self._broker = coalesce(broker, config.get("MQTT_BROKER"))
+        self._port = coalesce(port, config.get("MQTT_PORT"), ClientConfig.DEF_PORT)
+        self._user = coalesce(user, config.get("MQTT_USER"))
+        self._password = coalesce(password, config.get("MQTT_PASSWORD"))
+
+        self._prefix = coalesce(config.get("MQTT_PREFIX"), ClientConfig.DEF_PREFIX)
+
+    @property
+    def broker(self):
+        return self._broker    
+
+    @property
+    def port(self):
+        return self._port
     
-client.set_message(MoistureMessage)
-client.connect()
+    @property
+    def user(self):
+        return self._user
+    
+    @property
+    def password(self):
+        return self._password
+    
+    @property
+    def prefix(self):
+        return self._prefix
+    
+    def has_user_pass_auth(self):
+        return self.user is not None and self.password is not None
+    
+
+class Client:
+    def __init__(self, config: Optional[ClientConfig] = None):
+        if config is None:
+            config = ClientConfig()
+
+        self._host = config.broker
+        self._port = config.port 
+        self._prefix = config.prefix 
+
+        self.client = self.get_mqtt_connected_client(config)
+
+    @property
+    def client_id(self):
+        return self._client_id
+    
+    @staticmethod
+    def _on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            logging.info("Connected to MQTT Broker!")
+        else:
+            logging.error(f"Failed to connect, return code {rc}")
+
+    def get_mqtt_connected_client(self, config: ClientConfig, use_ssl: bool=False):
+        client_id = f"{socket.gethostname()}-{random.randint(0, 1000)}"
+
+        client = mqtt_client.Client(client_id)
+        
+        if config.has_user_pass_auth():
+            client.username_pw_set(config.user, config.password)
+
+        if use_ssl:
+            client.tls_set(certfile=None,
+                keyfile=None,
+                cert_reqs=ssl.CERT_REQUIRED)
+
+        client.on_connect = Client._on_connect
+
+        client.connect(config.broker, config.port)
+
+        return client
+    
+    @staticmethod
+    def publish_message(client, topic, message, delay):
+        while True:
+            if callable(message):
+                message = message()
+
+            r = client.publish(topic, message)
+
+            time.sleep(delay)
+
+            #TODO: add signeled exit
+            #TODO: publish_message should just publish message, the "looping logic" should be in a separate method
+
+    def run(self, messages):
+        client = self.client
+        threads = []  
+
+        client.loop_start()  
+        for msg in messages:
+            #TODO: refacator StandardMessage class and child classes
+            topic = f"{self._prefix}{msg.message}/latest"
+            message = msg.get_response
+            delay = msg.delay
+            thread = threading.Thread(target=Client.publish_message, args=(client, topic, message, delay))
+            threads.append(thread)
+            thread.start() 
+        client.loop_stop()
+
+if __name__ == '__main__':
+    from rMoistPi.message.moisture_message import MoistureMessage
+    c = Client()
+
+    c.run([MoistureMessage])
--- rMoistPi/socket_client.py
+++ rMoistPi/socket_client.py
@@ -1,94 +1,133 @@
+import os
+from dotenv import dotenv_values
+import random
+
+from functools import reduce 
+
+from typing import Optional
+
+from paho.mqtt import client as mqtt_client
+
 import socket
+import ssl
+
 import logging
 
-from time import sleep
+import datetime
 
-from rMoistPi.message import StandardMessage
-from rMoistPi.exceptions import CloseConnection
+import threading
+
+logging.basicConfig(level=logging.INFO)
+
+def coalesce(*arg):
+  return reduce(lambda x, y: x if x is not None else y, arg)
 
 
-class SocketClient():
-    DEFAULT_PORT = 2137
-    MAX_RETRIES = 3
-    RETRY_DELAY = 15
+class ClientConfig:
+    DEF_PORT = 1883
+    DEF_PREFIX = "home/iot/"
 
-    LOG_LEVEL = logging.INFO
+    def __init__(self, 
+                 broker: Optional[str] = None, 
+                 port: Optional[int] = None, 
+                 user: Optional[str] = None, 
+                 password: Optional[str] = None):
+        
+        config = {
+            **os.environ,
+            **dotenv_values(".env")
+        }
 
-    def __init__(self, host: str, port: int = DEFAULT_PORT, log_level: int = LOG_LEVEL):
-        self._host = host
-        self._port = port
+        self._broker = coalesce(broker, config.get("MQTT_BROKER"))
+        self._port = coalesce(port, config.get("MQTT_PORT"), ClientConfig.DEF_PORT)
+        self._user = coalesce(user, config.get("MQTT_USER"))
+        self._password = coalesce(password, config.get("MQTT_PASSWORD"))
 
-        self._msg_library = None
-
-        self._connection_closed = False
-
-        logging.basicConfig(
-            format="%(asctime)s %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-            level=log_level,
-        )
+        self._prefix = coalesce(config.get("MQTT_PREFIX"), ClientConfig.DEF_PREFIX)
 
     @property
-    def host(self):
-        return self._host
+    def broker(self):
+        return self._broker    
 
     @property
     def port(self):
         return self._port
+    
+    @property
+    def user(self):
+        return self._user
+    
+    @property
+    def password(self):
+        return self._password
+    
+    @property
+    def prefix(self):
+        return self._prefix
+    
+    def has_user_pass_auth(self):
+        return self.user is not None and self.password is not None
+    
+
+class Client:
+    def __init__(self, config: Optional[ClientConfig] = None):
+        if config is None:
+            config = ClientConfig()
+
+        self._host = config.broker
+        self._port = config.port 
+        self._prefix = config.prefix 
+
+        self.client = self.get_mqtt_connected_client(config)
 
     @property
-    def length(self):
-        return self._length
+    def client_id(self):
+        return self._client_id
     
-    def set_message(self, msg: StandardMessage):
-        self._msg = msg
+    @staticmethod
+    def _on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            logging.info("Connected to MQTT Broker!")
+        else:
+            logging.error(f"Failed to connect, return code {rc}")
+
+    def get_mqtt_connected_client(self, config: ClientConfig, use_ssl: bool=False):
+        client_id = f"{socket.gethostname()}-{random.randint(0, 1000)}"
+
+        client = mqtt_client.Client(client_id)
+        
+        if config.has_user_pass_auth():
+            client.username_pw_set(config.user, config.password)
+
+        if use_ssl:
+            client.tls_set(certfile=None,
+                keyfile=None,
+                cert_reqs=ssl.CERT_REQUIRED)
+
+        client.on_connect = Client._on_connect
+
+        client.connect(config.broker, config.port)
+
+        return client
     
-    def connect(self):
-        c = (self.host, self.port)
+    @staticmethod
+    def publish_message(client, message):
+        topic = message.message
+        message = message.get_response()
 
-        retry = 0
+        r = client.publish(topic, message)
 
-        while retry < SocketClient.MAX_RETRIES and not self._connection_closed:
-            try:
-                with socket.socket() as client_socket:
-                    client_socket.connect(c)
+    def run(self, messages):
+        self.client.loop_start()  
+        threads = []     
+        for message in messages:
+           thread = threading.Thread(target=Client.publish_message, args=(self.client, "home/iot/latest", message))
+           threads.append(thread)
+           thread.start() 
+        self.client.loop_stop()
 
-                    connection = Connection(client_socket, self._msg)
-                    connection.run()
-            except CloseConnection as ex:
-                logging.error(ex)
-                break
-            except:
-                retry += 1
-                retry_delay = SocketClient.RETRY_DELAY*retry
-                logging.error(f"Connection error. Retry {retry} from {SocketClient.MAX_RETRIES} in {retry_delay} seconds.")
-                sleep(retry_delay)
+if __name__ == '__main__':
+    from rMoistPi.message.moisture_message import MoistureMessage
+    c = Client()
 
-    
-class Connection():
-    LENGTH = 1024
-
-    def __init__(self, socket, message: StandardMessage):
-        self._socket = socket
-        self._msg = message
-
-    def run(self):
-        try:
-            while True:
-                self._socket.send(self._msg.message.encode()) 
-                data = self._socket.recv(Connection.LENGTH).decode()
-
-                if data == 'shutdown':
-                    raise CloseConnection("Connection closed by a server")
-                
-                if not data:
-                    raise CloseConnection("Connection closed. Cliend did not revieved any data")
-
-
-                print('Received from server: ' + data)
-
-                sleep(self._msg.delay)
-        except KeyboardInterrupt:
-            raise CloseConnection("Connection closed by a client")
-
-
+    c.run([MoistureMessage])
